c680f346c0982cba3d4625621c2769463d933cdd
Superpol@ris <sallamy2580@gmail.com>
2022-03-21 02:56:13 +0530

    fix(mongoose): reuse db connection

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -1,24 +1,42 @@
 import mongoose from 'mongoose';
 
-const connectDB = async () => {
-  if (mongoose.connections[0].readyState) {
-    console.log('already connected');
-    return;
+const MONGODB_URI = process.env.MONGODB_URI
+
+if (!MONGODB_URI) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  )
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null }
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn
   }
 
-  mongoose
-    .connect(process.env.MONGODB_URI,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // useFindAndModify: false,
-        // useCreateIndex: true,
-      }
-    )
-    .then(() => console.log('database started'))
-    .catch((err) => console.log(err));
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
 
-  //
-};
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose
+    })
+  }
+  cached.conn = await cached.promise
+  return cached.conn
+}
 
 export default connectDB;
