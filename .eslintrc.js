cfcb11e5b959a04ed874cd1b3784a72505d52c49
Superpol@ris <sallamy2580@gmail.com>
2022-03-25 03:20:29 +0530

    style(lint): add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,37 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
-  rules: {},
+  plugins: ['react', 'simple-import-sort', 'import'],
+  rules: {
+    'sort-vars': ['error', { ignoreCase: false }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-unused-modules': [
+      1,
+      { unusedExports: true, ignoreExports: ['**/api/*'] },
+    ],
+    'react/function-component-definition': [
+      1,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+  },
 }
